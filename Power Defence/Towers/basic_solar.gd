2658151b33ed3965dfffd1ef1306151b7987d246
Paulb23 <p_batty@hotmail.co.uk>
2017-07-29 18:43:39 +0100

    art and animations for solar tower

--- Power Defence/Towers/basic_solar.gd
+++ Power Defence/Towers/basic_solar.gd
@@ -10,13 +10,18 @@ var powered = true
 var generating = false
 var ready = false
 
+
+var animation
+
 func _ready():
 	get_node("generator_timer").connect("timeout", self, "generated");
+	animation = get_node("AnimationPlayer")
 	pass
 	
 func _fixed_process(delta):
 	if (!powered && generating):
 		generating = false
+		animation.play("power off")
 	
 	if (!activated || !powered):
 		return
@@ -25,17 +30,20 @@ func _fixed_process(delta):
 		generating = true
 		get_node("generator_timer").set_wait_time(rand_range(5, 10))
 		get_node("generator_timer").start()
+		animation.play("generating")
 		
 func generated():
 	if (!activated || !powered):
 		return 
 	generating = false
 	ready = true
+	animation.play("created")
 	get_node("Sprite").set_frame(6)
 	
 func collect():
 	ready = false
 	get_node("Sprite").set_frame(0)
+	animation.play("power off")
 
 func activate():
 	activated = true
