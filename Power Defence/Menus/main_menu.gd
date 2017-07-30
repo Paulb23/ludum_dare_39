b635b0ac071b1c466889200ef82c7027aa5716a5
Paulb23 <p_batty@hotmail.co.uk>
2017-07-30 11:23:16 +0100

    Fished main menu

--- Power Defence/Menus/main_menu.gd
+++ Power Defence/Menus/main_menu.gd
@@ -3,6 +3,8 @@ extends Node2D
 func _ready():
 	get_node("play").connect("pressed", self, "play")
 	get_node("exit").connect("pressed", self, "exit")
+	get_node("play").connect("mouse_enter", self, "item_hover")
+	get_node("exit").connect("mouse_enter", self, "item_hover")
 	get_node("music_vol").connect("value_changed", self, "music_vol")
 	get_node("sfx_vol").connect("value_changed", self, "sfx_vol")
 	
@@ -10,6 +12,9 @@ func _ready():
 	get_node("sfx_vol").set_value(AudioServer.get_fx_global_volume_scale())
 
 func play():
+	get_node("SamplePlayer").play("menu_select")
+	while get_node("SamplePlayer").is_processing():
+		pass
 	get_node("/root/globals").set_scene("res://Levels/test_level.tscn")
 	
 func music_vol(val):
@@ -21,4 +26,10 @@ func sfx_vol(val):
 	pass
 	
 func exit():
+	get_node("SamplePlayer").play("menu_select")
+	while get_node("SamplePlayer").is_processing():
+		pass
 	get_tree().quit()
+	
+func item_hover():
+	get_node("SamplePlayer").play("menu_hover")
